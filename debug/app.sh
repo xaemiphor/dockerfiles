9cb579ca9f50468f0cb85dbc479ade935aa27ae3
Xaemiphor <Xaemiphor@users.noreply.github.com>
2023-12-21 16:42:01 -0800

    chore: fix formatting

--- debug/app.sh
+++ debug/app.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 function _header {
-	if [[ "${GITHUB_ACTIONS:-false}" == "true" ]]; then
-		echo "::group::${@}"
-	else
-		echo "== ${@}"
-	fi
+  if [[ "${GITHUB_ACTIONS:-false}" == "true" ]]; then
+    echo "::group::${@}"
+  else
+    echo "== ${@}"
+  fi
 }
 function _footer {
-	if [[ "${GITHUB_ACTIONS:-false}" == "true" ]]; then
-		echo "::endgroup::"
-	else
-		echo "=="
-	fi
+  if [[ "${GITHUB_ACTIONS:-false}" == "true" ]]; then
+    echo "::endgroup::"
+  else
+    echo "=="
+  fi
 }
 if [[ "${CI:-}" == "woodpecker" ]]; then
-	WOODPECKER=true
+  WOODPECKER=true
 fi
 _header "CI Env check"
 echo "CI: ${CI:-}"
@@ -30,7 +30,7 @@ _footer
 _header "Test Data 'FOO'"
 __VARS=( $(env | awk -F'=' 'tolower($1) ~ /foo/ && /=/{print $1}') )
 for entry in ${__VARS[@]}; do
-	echo "[variable] ${entry}"
-	echo "[value] ${!entry}"
+  echo "[variable] ${entry}"
+  echo "[value] ${!entry}"
 done
 _footer
