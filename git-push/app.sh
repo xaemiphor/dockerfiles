437a41f071a2f2d969aba089a47900fe9d4f4356
Xaemiphor <Xaemiphor@users.noreply.github.com>
2023-12-24 23:13:10 -0800

    git-push: add stdout note

--- git-push/app.sh
+++ git-push/app.sh
@@ -53,6 +53,7 @@ git config pull.ff only
 git pull --quiet origin "${__commit_branch}"
 # TODO Add some sort of support for globbing/include/exclude/etc here
 if [[ $(git status -s | grep '^[AM?]' | wc -l) -gt 0 ]]; then
+  echo "Additions or modifications noted, committing and pushing to repository."
   git add . # Adds all, may eventually need to figure out globbing solution as referenced
   git config user.name "${__commit_user}"
   git config user.email "${__commit_email}"
