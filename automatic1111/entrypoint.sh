b03a021714c845869cfbf9cd10e5900cad1c3950
Xaemiphor <Xaemiphor@users.noreply.github.com>
2024-01-08 21:27:29 -0800

    a1111: give up simlifying data structures, just package and doc the paths

--- automatic1111/entrypoint.sh
+++ automatic1111/entrypoint.sh
@@ -2,56 +2,41 @@
 set -e
 _ARGS=( '--port' "${PORT:-7860}" )
 
-if [[ ! -e "/config/config.json" ]]; then
-  if [[ -L "/app/config.json" ]]; then
-    rm /app/config.json
-  fi
-  if [[ ! -e "/app/config.json" ]]; then
-    cd /app
-    python3 -c 'from modules import shared, shared_init; shared_init.initialize(); shared.opts.save(shared.config_filename)'
-    mv /app/config.json /config/config.json
-  fi
-fi
-if [[ -f "/config/config.json" ]]; then
-  if [[ ! -L "/app/config.json" ]]; then
-    if [[ -e "/app/config.json" ]]; then
+if ! mountpoint -q /app/config.json; then
+  if [[ ! -e "/config/config.json" ]]; then
+    if [[ -L "/app/config.json" ]]; then
       rm /app/config.json
     fi
-    ln -s /config/config.json /app/config.json
+    if [[ ! -e "/app/config.json" ]]; then
+      cd /app
+      python3 -c 'from modules import shared, shared_init; shared_init.initialize(); shared.opts.save(shared.config_filename)'
+      mv /app/config.json /config/config.json
+    fi
+  fi
+  if [[ -f "/config/config.json" ]]; then
+    if [[ ! -L "/app/config.json" ]]; then
+      if [[ -e "/app/config.json" ]]; then
+        rm /app/config.json
+      fi
+      ln -s /config/config.json /app/config.json
+    fi
   fi
 fi
 
-# Move some paths to /data
-for entry in 'models' 'outputs' 'log' 'extensions' 'embeddings'; do
-  if [[ ! -d "/data/${entry}" ]]; then
-    if [[ -d "/app/${entry}" ]]; then
-      mv "/app/${entry}" "/data/${entry}"
-    else
-      mkdir "/data/${entry}"
-    fi
-  fi
-  if [[ ! -L "/app/${entry}" ]]; then
-    if [[ -e "/app/${entry}" ]]; then
-      rm -R "/app/${entry}"
-    fi
-    ln -s "/data/${entry}" "/app/${entry}"
-  fi
-done
-
 # Preconfigure config.json
-if [[ -f "/config/config.json" ]]; then
+if [[ -e "/app/config.json" && ! -d "/app/config.json" ]]; then
   # Load config arguments passed as CFG__ env vars
   for ENV_VAR in $(env | awk -F '=' '/^CFG__/{print $1}' | sort); do
     var=$(echo "${ENV_VAR,,}" | sed 's/^cfg__//g')
     value="${!ENV_VAR}"
     re='^[-]?[0-9]+([.][0-9]+)?$'
     if [[ "${value,,}" == "true" || "${value,,}" == "false" || ${value} =~ ${re} ]]; then
-      jq --arg var "$var" --argjson val "${value}" '.[$var] = $val' /config/config.json > /tmp/config.json.modified
-      mv /tmp/config.json.modified /config/config.json
+      jq --arg var "$var" --argjson val "${value}" '.[$var] = $val' /app/config.json > /tmp/config.json.modified
     else
-      jq --arg var "$var" --arg val "${!ENV_VAR}" '.[$var] = $val' /config/config.json > /tmp/config.json.modified
-      mv /tmp/config.json.modified /config/config.json
+      jq --arg var "$var" --arg val "${!ENV_VAR}" '.[$var] = $val' /app/config.json > /tmp/config.json.modified
     fi
+    cat /tmp/config.json.modified > /app/config.json
+    rm /tmp/config.json.modified
   done
 fi
 
@@ -69,9 +54,12 @@ for _arg in $(env | awk -F '=' '/^ARG__/{print $1}' | sort); do
   fi
 done
 
+# Restore files that may have gone missing due to docker mounts
+git status -s | awk '$1 ~ /^D/{print $NF}' | xargs git restore
+
 # Install any deps from extensions
-find /data/extensions/ -maxdepth 2 -type f -name 'requirements.txt' -exec pip install -r {} \;
-PYTHONPATH=/app find /data/extensions/ -maxdepth 2 -type f -name 'install.py' -exec python {} \;
+find /app/extensions/ -maxdepth 2 -type f -name 'requirements.txt' -exec pip install -r {} \;
+PYTHONPATH=/app find /app/extensions/ -maxdepth 2 -type f -name 'install.py' -exec python {} \;
 
 cd /app
 python ${SCRIPT:-webui.py} ${_ARGS[@]}
