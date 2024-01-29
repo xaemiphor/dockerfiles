62fecfc27a236a4b798237d139e7098333e14daf
Xaemiphor <xaemiphor@users.noreply.github.com>
2024-01-29 09:22:19 -0800

    litellm-ollama: add svc dependencies, port ollama_pull startup to s6

--- litellm-ollama/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ollama serve &
-(sleep 2s && if [[ -n "${OLLAMA_PULL:-}" ]]; then echo -n "${OLLAMA_PULL:-}" | xargs -n1 -P1 -d ',' ollama pull ; fi ) &
-litellm --port ${LITELLM_PORT:-8000} --host "${LITELLM_HOST:-0.0.0.0}"
