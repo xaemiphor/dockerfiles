bb836dcb5be3daeeea3704625edd4776cb1427d7
Xaemiphor <xaemiphor@users.noreply.github.com>
2024-03-08 13:37:48 -0800

    a1111: simplifying entrypoint, just use compose command args

--- automatic1111/entrypoint.sh
+++ automatic1111/entrypoint.sh
@@ -5,62 +5,23 @@ if [[ -n "${CONTAINER_DEBUG:-}" ]]; then
 fi
 _ARGS=( '--port' "${PORT:-7860}" )
 
+CONFIG_DIR=${CONFIG_DIR:-/config}
+
 # Fix ownerships
-for x in '/config' '/app'; do
+for x in "${CONFIG_DIR}" '/app'; do
   chown -R 1000:1000 ${x}
 done
 
-if ! mountpoint -q /app/config.json; then
-  if [[ ! -e "/config/config.json" ]]; then
-    if [[ -L "/app/config.json" ]]; then
-      rm /app/config.json
-    fi
-    if [[ ! -e "/app/config.json" ]]; then
-      cd /app
-      su user -c "/app/venv/bin/python3 -c 'from modules import shared, shared_init; shared_init.initialize(); shared.opts.save(shared.config_filename)'"
-      mv /app/config.json /config/config.json
-    fi
-  fi
-  if [[ -f "/config/config.json" ]]; then
-    if [[ ! -L "/app/config.json" ]]; then
-      if [[ -e "/app/config.json" ]]; then
-        rm /app/config.json
-      fi
-      ln -s /config/config.json /app/config.json
-    fi
-  fi
-fi
+# Create config file if not exists
+#if [[ ! -e "${CONFIG_DIR}/config.json" ]]; then
+#  cd /app
+#  su user -c "/app/venv/bin/python3 -c 'from modules import shared, shared_init; shared_init.initialize(); shared.opts.save(shared.config_filename)'"
+#  mv /app/config.json "${CONFIG_DIR}/config.json"
+#fi
+_ARGS+=( '--ui-settings-file' "${CONFIG_DIR}/config.json" )
+_ARGS+=( '--ui-config-file' "${CONFIG_DIR}/ui-config.json" )
+export SD_WEBUI_CACHE_FILE="${CONFIG_DIR}/cache.json"
 
-# Preconfigure config.json
-if [[ -e "/app/config.json" && ! -d "/app/config.json" ]]; then
-  # Load config arguments passed as CFG__ env vars
-  for ENV_VAR in $(env | awk -F '=' '/^CFG__/{print $1}' | sort); do
-    var=$(echo "${ENV_VAR,,}" | sed 's/^cfg__//g')
-    value="${!ENV_VAR}"
-    re='^[-]?[0-9]+([.][0-9]+)?$'
-    if [[ "${value,,}" == "true" || "${value,,}" == "false" || ${value} =~ ${re} ]]; then
-      jq --arg var "$var" --argjson val "${value}" '.[$var] = $val' /app/config.json > /tmp/config.json.modified
-    else
-      jq --arg var "$var" --arg val "${!ENV_VAR}" '.[$var] = $val' /app/config.json > /tmp/config.json.modified
-    fi
-    cat /tmp/config.json.modified > /app/config.json
-    rm /tmp/config.json.modified
-  done
-fi
-
-for _arg in $(env | awk -F '=' '/^ARG__/{print $1}' | sort); do
-  var=$(echo "${_arg,,}" | sed -e 's/^arg__//g' -e 's/_/-/g')
-  value="${!_arg}"
-  if [[ -z "${value}" ]]; then
-    _ARGS+=( "--${var}" )
-  elif [[ "${value,,}" == "true" || "${value,,}" == "false" ]]; then
-    _ARGS+=( "--${var}" "${value,,}" )
-  elif [[ -n "${value}" ]]; then
-    _ARGS+=( "--${var}" "${value}" )
-  else
-    echo "[ENTRYPOINT]: ERROR - ${_arg} / ${var} / ${value} - Not sure what these are"
-  fi
-done
 
 # Restore files that may have gone missing due to docker mounts
 if [[ $(cd /app ; git status -s | awk '$1 ~ /^D/{for (i=2; i<NF; i++) printf $i " "; print $NF}' | wc -l) -gt 0 ]]; then
@@ -73,5 +34,5 @@ fi
 #PYTHONPATH=/app find /app/extensions/ -maxdepth 2 -type f -name 'install.py' -exec python {} \;
 
 cd /app
-echo "== $(date -u) Starting with args ${SCRIPT:-webui.sh} ${_ARGS[@]}"
-su user -c "bash ${SCRIPT:-webui.sh} ${_ARGS[@]}"
+echo "== $(date -u) Starting with args ${SCRIPT:-webui.sh} ${_ARGS[@]} $@"
+su user -c "bash ${SCRIPT:-webui.sh} ${_ARGS[@]} $@"
